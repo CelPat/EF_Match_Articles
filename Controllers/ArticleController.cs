ec1a1631d6d380f569d18666eb38a130e239d5d0
Patryk <68706797+CelPat@users.noreply.github.com>
2024-04-13 17:34:35 +0200

    Controllers

---
+++ Controllers/ArticleController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ASWWW_lab2_gr4_nohttps.Models;
+
+namespace ASWWW_lab1_gr4_nohttps.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ArticleController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index(int id) 
+        {
+            var article = _dbContext.Articles.FirstOrDefault(x => x.Id == id); //Repository.Articles.ToList()[id];
+            if(article != null) { 
+                return View(article);
+            }
+            return View("Error");
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Article article)
+        {
+            if(ModelState.IsValid)
+            {
+                _dbContext.Articles.Add(article); // Repository.AddArticle(article)
+                _dbContext.SaveChanges();
+                return View("Added", article);
+            }
+            return View("Error");
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using ASWWW_lab2_gr4_nohttps.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASWWW_lab1_gr4_nohttps.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public HomeController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Home";
+
+            var articles = _dbContext.Articles!.ToList(); // var articles = Repository.articles;
+            return View(articles);
+        }
+    }
+}
